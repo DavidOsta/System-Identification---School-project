0b41010a6914bdcdf2adb3e92c2f850002c11c44
Dave <ostadavi@gmail.com>
2017-03-24 11:51:46 +0100

    fix windows bug in static function sav_data

--- src/static functions/save_data.m
+++ src/static functions/save_data.m
@@ -4,7 +4,7 @@ function file_path = save_data(measured_data,folder_path, filename)
 %   measured_data - data to save
 
 % check if an argument exists
-time_stamp = strrep(datestr(now), '_', '');
+time_stamp = strrep(datestr(now), ':', '_');
 if ~exist('filename','var')
     filename = time_stamp;
 else
@@ -19,22 +19,3 @@ end
 file_path = [folder_path, '/', filename];
 save(file_path, 'measured_data');
 end
-
-
-%         function save_data(self,name_arg)
-%             %Save measured results
-%             %   save folder - save folder destination
-%             %   measured_data - data to save
-%             
-%             prefix = 'friction_measurement_' ;
-%             if ~exist('name_arg','var')
-%                 timestamp = strrep(datestr(now), ':', '_');
-%                 filename = [prefix, timestamp];
-%             else
-%                 filename = [prefix, name_arg];
-%             end
-%             
-%             file_path = [self.save_folder, '/', filename];
-%             simout = self.simout;
-%             save(file_path, 'simout');
-%         end
